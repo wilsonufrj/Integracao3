9e031a441e50097e2e34cfe2d1bf8eeb1b407052
Danilo Collares <dundeepessoal@gmail.com>
2019-07-20 16:22:25 -0300

    Modelo final integracao 2

---
+++ Ionic/src/app/form/form.page.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { MemeService } from '../meme.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.page.html',
+  styleUrls: ['./form.page.scss'],
+})
+export class FormPage implements OnInit {
+
+  meme;
+  searchtext: string;
+  form = {}
+  constructor(
+    public memeService: MemeService,
+    public router: Router) { }
+
+  getMeme(id):void{
+    console.log(id);
+    this.memeService.getMeme(id).subscribe(
+      (res)=>{
+        this.meme = res[0];
+        this.form = this.meme;
+    });
+  }
+  ngOnInit() {
+  }
+
+  onSubmit(){ 
+    console.log(this.form)
+    switch(this.form.method){
+      case "post": 
+        console.log('postititpoo');
+        this.memeService.createMeme(this.form).subscribe((res)=>{console.log(res);});
+        break;
+      case "put":
+        console.log('putititpoo');
+        this.memeService.updateMeme(this.form, this.meme.id).subscribe((res)=>{console.log(res);});
+        break;
+      case"delete":
+        console.log('deletetipoo');
+        this.memeService.deleteMeme(this.form, this.meme.id).subscribe((res)=>{console.log(res);});
+        break;
+    }
+    this.router.navigate(['tabs/tab2']);
+  }
+}
