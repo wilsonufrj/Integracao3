b74b303420dc4ae9d1c65db9db0ea3ac5446590c
Danilo Collares <dundeepessoal@gmail.com>
2019-07-24 16:04:57 -0300

    seeder atualizada

--- Laravel/database/seeds/MemeTableSeeder.php
+++ Laravel/database/seeds/MemeTableSeeder.php
@@ -15,28 +15,28 @@ class MemeTableSeeder extends Seeder
 			'nome' => 'Minecraft',
 			'descricao' => 'Esse jogo é uma porra',
 			'nivel'=> 5,
-			'imagem' => 'no',
+			'imagem' => 'https://images-na.ssl-images-amazon.com/images/I/512dVKB22QL.png',
 		]);
 
 		DB::table('memes')->insert([
 			'nome' => 'Gnome',
 			'descricao' => 'HELLO ME OLD CHUM',
 			'nivel'=> 10,
-			'imagem' => 'no',
+			'imagem' => 'https://i.kym-cdn.com/entries/icons/original/000/025/526/gnome.jpg',
 		]);
 
 		DB::table('memes')->insert([
 			'nome' => 'Vem Tranquilo',
 			'descricao' => 'Tá muito afobado',
 			'nivel'=> 3,
-			'imagem' => 'no',
+			'imagem' => 'https://s1.dmcdn.net/wNBFu/x1080-Jha.jpg',
 		]);
 
 		DB::table('memes')->insert([
 			'nome' => 'Pikachu surpreso',
 			'descricao' => ':O',
 			'nivel'=> 7,
-			'imagem' => 'no',
+			'imagem' => 'https://i.pinimg.com/280x280_RS/ca/cd/b6/cacdb6ab013e10beb2a9286f4ccd79b4.jpg',
 		]);
     }
 }
