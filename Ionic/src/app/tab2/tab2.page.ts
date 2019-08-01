fa084babc31087d7ad23bca57f00d89cfd074a7a
Danilo Collares <dundeepessoal@gmail.com>
2019-07-31 21:47:08 -0300

    *sussurro* CORS

--- Ionic/src/app/tab2/tab2.page.ts
+++ Ionic/src/app/tab2/tab2.page.ts
@@ -1,50 +1,45 @@
 import { Component } from '@angular/core';
-import { MemeService }  from '../meme.service';
-import { ModalController } from '@ionic/angular';
+import { MemeService } from '../meme.service';
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+	selector: 'app-tab2',
+	templateUrl: 'tab2.page.html',
+	styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  memes;
-  constructor(public memeService: MemeService)  {
-  }
+	memes;
+	constructor(public memeService: MemeService) {
+	}
 
 	getMemes(): void {
-    this.memeService.getMemes().subscribe(
-      (res) => {
-        console.log(res);
-        this.memes = res;
-        for(let meme in this.memes){
-          console.log(meme);
-        }
-      }
-    );
+		this.memeService.getMemes().subscribe(
+			(res) => {
+				window.location.href = res;
+			}
+		);
 	}
 
 	getMeme(id): void {
-			this.memeService.getMeme(id).subscribe(
-					(res) => {
-							console.log(res);
-					}
-			);
+		this.memeService.getMeme(id).subscribe(
+			(res) => {
+				console.log(res);
+			}
+		);
 	}
 
-	
+
 	updateMeme(meme): void {
-		this.memeService.updateMeme(meme,meme.id).subscribe(
+		this.memeService.updateMeme(meme, meme.id).subscribe(
 			(res) => {
-					console.log(res);
+				console.log(res);
 			}
 		);
 	}
 
 	deleteMeme(meme): void {
-		this.memeService.deleteMeme(meme,meme.id).subscribe(
+		this.memeService.deleteMeme(meme, meme.id).subscribe(
 			(res) => {
-					console.log(res);
+				console.log(res);
 			}
 		);
 	}
@@ -52,13 +47,13 @@ export class Tab2Page {
 	createMeme(meme): void {
 		this.memeService.createMeme(meme).subscribe(
 			(res) => {
-					console.log(res);
+				console.log(res);
 			}
 		);
 	}
 
-  ngOnInit(){
-  }
+	ngOnInit() {
+	}
 
 
 }
